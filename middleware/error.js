8b7853c509f6cb49366d9db64218c872b6795054
CouldBeFree <propokerstar90@gmail.com>
2019-10-19 19:23:11 +0300

    Handle errors

---
+++ middleware/error.js
@@ -0,0 +1,20 @@
+const errorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, rea, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+  
+  console.log(err.stack);
+  
+  if(err.name === 'CastError'){
+    const message = `Bootcamp not found with id of ${err.value}`;
+    error = new errorResponse(message, 404);
+  }
+  
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server error'
+  })
+};
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const errorHandler = require('./middleware/error');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
@@ -23,6 +24,8 @@ app.use(express.json());
 
 //Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5050;
 

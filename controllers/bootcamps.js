1153c6ef9ad82321350223f6eaa2aef1285784fa
CouldBeFree <propokerstar90@gmail.com>
2019-10-21 22:00:23 +0300

    Filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,9 +7,35 @@ const geocoder = require('../utils/geocoder');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
   
-  res.status(200).json({ success: true, data: bootcamps });
+  const reqQuery = { ...req.query };
+  const removeFields = ['select', 'sort'];
+  removeFields.forEach(params => delete reqQuery[params]);
+  
+  let queryStr = JSON.stringify(reqQuery);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+  
+  // Find resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+  
+  //Select fields
+  if(req.query.select){
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+  
+  // Sort fields
+  if(req.query.sort){
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt')
+  }
+  
+  const bootcamps = await query;
+  
+  res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 });
 
 // @desc    Get single bootcamp

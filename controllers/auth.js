0e54013a376747b7bcd6129c72242dfc70985b77
CouldBeFree <propokerstar90@gmail.com>
2019-10-26 13:58:36 +0300

    Login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,3 +22,35 @@ exports.register = asyncHandler(async (req, res, next) => {
     token
   })
 });
+
+// @desc    Login user
+// @route   POST /api/v1/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { password, email } = req.body;
+  
+  // Validate
+  if(!password || !email){
+    return next(new errorResponse('Please provide an email & password', 400))
+  }
+  
+  // Check for user
+  const user = await User.findOne({email}).select('+password');
+  
+  if(!user){
+    return next(new errorResponse('Invalid credentials', 401))
+  }
+  
+  const isMatch = await user.matchPassword(password);
+  
+  if(!isMatch){
+    return next(new errorResponse('Invalid credentials', 401))
+  }
+  
+  const token = user.getSignedJwtToken();
+  
+  res.status(200).json({
+    success: true,
+    token
+  })
+});
--- models/User.js
+++ models/User.js
@@ -39,10 +39,16 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// Sign JWT and return
 userSchema.methods.getSignedJwtToken = function(){
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE
   })
+};
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function(password){
+  return await bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);

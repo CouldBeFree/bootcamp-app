1e8d541e677c38d5bdf3886c02f141294e1bc07a
sasha <propokerstar90@gmail.com>
2019-10-28 19:24:05 +0200

    Review CRUD

--- middleware/error.js
+++ middleware/error.js
@@ -8,7 +8,7 @@ const errorHandler = (err, rea, res, next) => {
   
   // Mongoose bad object ID
   if(err.name === 'CastError'){
-    const message = `Bootcamp not found with id of ${err.value}`;
+    const message = `Resource not found with id of ${err.value}`;
     error = new errorResponse(message, 404);
   }
   
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -15,10 +15,13 @@ const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
 const router = express.Router();
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
